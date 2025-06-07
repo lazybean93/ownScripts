e9148be6f0ed295dec5ad623edbcd603030a7d59
Oliver Tiebe <srvrdmn127@gmail.com>
2025-06-07 16:45:42 +0200

    Update

--- aufräumen.sh
+++ aufräumen.sh
@@ -1,4 +1,4 @@
-find . -type f -printf '%s %p\n' | sort -nr | grep -v '\.iso$\|\.mp4$\|\.mkv$\|\.zip$\|\.vdi$\|\.ova$\|\.exe$\|\.vmdk$\|\.MP4$\|\.img$\|\.MTS$\|\.sav$\|\.7z$\|\.tar$\|\.deb$\|\.gz$\|\.rar$\|/.secret/\|\.pptx$\|\.mp3$\|\.webm$\|\.xz$\|\.xcf$\|\.pdf$\|\.db$\|\.png$\|\.jar$\|\.MOV$\|\.mov$\|\.csv$\|\.tz$\|\.pack$\|\.journal$\|\.dat$\|\.xml$\|\.tgz$\|\.jpg$\|\.sqlite$\|\.txt$\|\.odt$\|\.log$\|\.wav$\|\.VHD$\|\.mpg$\|\.avi$\|\.WAV$\|\.wmv$\|\.wma$' | head -n 20
+find . -type f -printf '%s %p\n' | sort -nr | grep -v '\.iso$\|\.mp4$\|\.mkv$\|\.zip$\|\.vdi$\|\.ova$\|\.exe$\|\.vmdk$\|\.MP4$\|\.img$\|\.MTS$\|\.sav$\|\.7z$\|\.tar$\|\.deb$\|\.gz$\|\.rar$\|/.secret/\|\.pptx$\|\.mp3$\|\.webm$\|\.xz$\|\.xcf$\|\.pdf$\|\.db$\|\.png$\|\.jar$\|\.MOV$\|\.mov$\|\.csv$\|\.tz$\|\.pack$\|\.journal$\|\.dat$\|\.xml$\|\.tgz$\|\.jpg$\|\.sqlite$\|\.txt$\|\.odt$\|\.log$\|\.wav$\|\.VHD$\|\.mpg$\|\.avi$\|\.WAV$\|\.wmv$\|\.wma$\|\.pkg$\|\.ZIP$\|\.PUP$\|\.opus$\|\.m4a$' | head -n 20
 
 #find . -type f -name *.dll | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
 #find . -type f -name *.reg | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
@@ -13,4 +13,9 @@ find . -type f -printf '%s %p\n' | sort -nr | grep -v '\.iso$\|\.mp4$\|\.mkv$\|\
 #find . -type f -name *.pyc | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
 #find . -type f -name *.dd | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
 #find . -type f -name *.ico | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
-#echo "rm deleteMe.sh" >> deleteMe.sh
+#find . -type f -name *.nfo | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
+find . -type f -name *.out | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
+find . -type f -name *.sfv | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
+find . -type f -name *.md5 | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
+find . -type f -name *.url | sed 's/^/rm "/g;s/$/"/g;s/^rm ""$//g' >> deleteMe.sh
+echo "rm deleteMe.sh" >> deleteMe.sh
